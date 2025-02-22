0b081b4a421c04857f6c98e0817e61c816760fd4
fastbikes <66102652@qq.com>
2025-02-22 09:10:31 +0800

    fix chrome and chrome driver

--- test123.py
+++ test123.py
@@ -1,8 +1,14 @@
-<PYTHON>
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
-# 使用 Chrome 驱动
-driver = webdriver.Chrome()
+# 配置 Chrome 选项
+chrome_options = Options()
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_options.add_argument('--headless')  # 如果需要无头模式，可以添加这行
+
+# 启动 Chrome
+driver = webdriver.Chrome(options=chrome_options)
 
 # 打开网页
 driver.get("https://www.google.com")
